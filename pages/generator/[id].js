90ae38aef04f29d7ec45e250bb3dd8dc3ee4ce9a
Evhenyj Pasechnyj <evgenij9241@gmail.com>
2023-02-16 12:24:02 +0200

    test

--- pages/generator/[id].js
+++ pages/generator/[id].js
@@ -6,15 +6,54 @@ export const config = {
 }
 
 export default function Field({field}) {
+    if(Object.keys(field.fields).length === 0) return <p>no data</p>
     return (
         <>
-          <p>{field}</p>
+            <p>{field.id}</p>
+          {/*  <table style={{"width": "100%"}}>
+                <tr>
+                    <td style={{"width": "25%"}}><strong><font color="red">Special Note</font></strong></td>
+                    <td style={{"width": "75%"}}>{field.fields["User Requirements"]}</td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Conversion Action</strong></td>
+                    <td style={{"width": "75%"}}>{field.fields["Conversion Action"]}</td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Simple Description</strong></td>
+                    <td style={{"width": "75%"}}>{field.fields["User Offer"]}</td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Detailed Description</strong></td>
+                    <td style={{"width": "75%"}}>{field.fields["Detailed Description"]}</td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Advertiser Details</strong></td>
+                    <td style={{"width": "75%"}}>{field.fields["About Company"]}</td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Creatives / Assets</strong></td>
+                    <td style={{"width": "75%"}}>
+                        <a
+                            href={field.fields["Creatives Folder"]}
+                            target="_blank" rel="noreferrer">Available
+                            at this link</a></td>
+                </tr>
+                <tr>
+                    <td style={{"width": "25%"}}><strong>Sample Blog Post</strong></td>
+                    <td style={{"width": "75%"}}>
+                        <a href={field.fields["Example Posts"]}
+                           target="_blank" rel="noreferrer">
+                            Good Driver? Prove It, and This Insurance Company Will Give You a Lower Rate</a></td>
+                </tr>
+            </table>*/}
         </>
     )
 }
 
 export async function getStaticProps(context) {
-    const field = 'asdasdasd'
+    console.log(context.params.id + '----------------------')
+    const field = await getOneField(context.params.id)
     return {
         props: {
             field
