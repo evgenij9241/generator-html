5f6cde4ae16f750eee47f4ff0a2616e6dc07ef87
Evhenyj Pasechnyj <evgenij9241@gmail.com>
2023-02-16 13:35:54 +0200

    added copy functional

--- pages/generator/index.js
+++ pages/generator/index.js
@@ -2,29 +2,107 @@ import {useEffect, useState} from "react";
 import {getAllData, getOneField} from "@/services/getData";
 import styles from "./Generated.module.css"
 
-export default function Generated({data}) {
+export default function Generated() {
+    const [data, setData] = useState(null)
+    const [showMessages, setShowMessages] = useState(false)
+    useEffect(() => {
+        getAllData().then((data) => setData(data))
+    }, [])
+    const copyText = (html) => {
+        navigator.clipboard.writeText(html).then(
+            () => {
+                setShowMessages((prev) => !prev)
+                setTimeout(() => setShowMessages((prev) => !prev), 500)
+            }
+        )
+    }
     if (data === null) return <div>Loading...</div>
     return (
-        <ul className={styles.list}>
-            {data?.records.map((item) => {
+        <>
+            {
+                showMessages && <div
+                    style={{
+                    position: "fixed",
+                    right: 0,
+                    top: 0,
+                    background: "#278c27",
+                    color: "#fff",
+                    padding: "20px 40px",
+                }}>Copied to your clipboard</div>
 
-                return (
-                    <li key={item.id}>
-                            <img height="40px" src={item.fields['Image']?.[0].url} alt=""/>
-                        <a href={`/generator/${item.id}`}>{item.fields["Campaign Name"]}</a>
-                    </li>
-                )
-            })
             }
-        </ul>
+            <ul className={styles.list}>
+                {data?.records.map((item) => {
+                    const html = `<html>
+                    <head>
+                        <style>
+                            table, td {
+                                border: 1px solid black;
+                                border-collapse: collapse;
+                            }
+                            th, td {
+                                padding: 5px;
+                                text-align: left;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                    <table style={{"width": "100%"}}>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong><font color="red">Special Note</font></strong></td>
+                        <td style={{"width": "75%"}}>${item.fields["User Requirements"]}</td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Conversion Action</strong></td>
+                        <td style={{"width": "75%"}}>${item.fields["Conversion Action"]}</td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Simple Description</strong></td>
+                        <td style={{"width": "75%"}}>${item.fields["User Offer"]}</td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Detailed Description</strong></td>
+                        <td style={{"width": "75%"}}>${item.fields["Detailed Description"]}</td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Advertiser Details</strong></td>
+                        <td style={{"width": "75%"}}>${item.fields["About Company"]}</td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Creatives / Assets</strong></td>
+                        <td style={{"width": "75%"}}>
+                        <a
+                                href=${item.fields["Creatives Folder"]}
+                        target="_blank" rel="noreferrer">Available
+                        at this link</a></td>
+                    </tr>
+                    <tr>
+                        <td style={{"width": "25%"}}><strong>Sample Blog Post</strong></td>
+                        <td style={{"width": "75%"}}>
+                        <a href=${item.fields["Example Posts"]}
+                        target="_blank" rel="noreferrer">
+                        Good Driver? Prove It, and This Insurance Company Will Give You a Lower Rate</a></td>
+                    </tr>
+                    </table>
+                    
+                    </body>
+                    </html>`
+                    if (Object.keys(item.fields).length === 0) return null
+                    return (
+                        <li key={item.id}>
+                            <img height="40px" src={item.fields['Image']?.[0].url} alt=""/>
+                            <a href={`/generator/${item.id}`}>{item.fields["Campaign Name"]}</a>
+                            <textarea style={{width: "1px", height: "1px", display: "none"}}>
+                          {html}
+                        </textarea>
+                            <button style={{margin: "0 0 0 20px", cursor: "pointer"}}
+                                    onClick={() => copyText(html)}>Copy html code
+                            </button>
+                        </li>
+                    )
+                })
+                }
+            </ul>
+        </>
     )
-}
-
-export async function getStaticProps(context) {
-    const data = await getAllData()
-    return {
-        props: {
-            data
-        },
-    }
 }
